eee3537635258e8a20ea900ddf0faef952882b13
Akintola94 <akinyeleakintola94@gmail.com>
2024-01-04 18:59:39 -0800

    argument

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+    import sys
+
+    num = len(sys.argv) - 1
+    if num == 0:
+        print("{} arguments.".format('0'))
+    elif num == 1:
+        print("{} argument:".format(num))
+    else:
+        print("{} arguments:".format(num))
+
+    for i in range(num):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
