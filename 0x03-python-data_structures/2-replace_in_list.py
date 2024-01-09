6eead8f78d473afc320d3ab85c20bb33bb4066be
Akintola94 <akinyeleakintola94@gmail.com>
2024-01-09 05:27:44 -0800

    Task 2

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    list_len = len(my_list)
+    idx_len = list_len - 1
+    if idx < 0:
+        return my_list
+    elif idx > idx_len:
+        return my_list
+    else:
+        my_list[idx] = element
+        return new_list
