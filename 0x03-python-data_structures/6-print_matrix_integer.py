be3494eba4b52461963f2baf5b51ffb2144f9e52
Akintola94 <akinyeleakintola94@gmail.com>
2024-01-09 13:00:33 -0800

    Task 6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    matrix_len = len(matrix)
+
+    for row in matrix:
+        items = len(row)
+        column = 0
+        for number in row:
+            if column < items - 1:
+                print("{:d}".format(number), end=" ")
+            else:
+                print("{:d}".format(number), end=" ")
+            column += 1
+        print()
